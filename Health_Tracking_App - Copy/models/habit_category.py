1fdd7d76e02cac5c86418d8361a010499b950fe8
Daisyjosh <daisypotes93@gmail.com>
2026-03-21 22:50:35 +0530

    Add project

--- Health_Tracking_App - Copy/models/habit_category.py
+++
@@ -1,4 +0,0 @@
-class HabitCategory:
-    def __init__(self,category_id,category_name):
-        self.category_id = category_id
-        self.category_name = category_name
--- Health_Tracking_App - Copy/models/habit_template.py
+++
@@ -1,4 +0,0 @@
-class HabitTemplate:
-    def __init__(self,template_id,template_name):
-        self.template_id = template_id
-        self.template_name = template_name
